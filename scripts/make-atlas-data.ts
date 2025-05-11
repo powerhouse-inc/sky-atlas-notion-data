ae217d8403dde590c85ebe827fd409635d8ff765
Yasiel Cabrera <yasiel9506@gmail.com>
2025-05-11 15:54:04 -0400

    uncomment code

--- scripts/make-atlas-data.ts
+++ scripts/make-atlas-data.ts
@@ -153,7 +153,7 @@ async function makeAtlasData(args: {
 
     // Check if required Notion page files exist
     const requiredNotionFiles = [
-      //`${notionPagesOutputPath}/${MASTER_STATUS}.json`,
+      `${notionPagesOutputPath}/${MASTER_STATUS}.json`,
     ];
 
     // Add Atlas page files to required files
